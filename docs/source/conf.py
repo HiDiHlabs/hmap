dd8a1755beb47f92f9a724a3bb1fefb86b822502
Matthias Bieg <matthias.bieg@gmail.com>
2021-02-19 10:51:53 +0100

    increase version to 0.9.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Matthias Bieg'
 author = 'Matthias Bieg'
 
 # The full version, including alpha/beta/rc tags
-release = '0.9.1'
+release = '0.9.2'
 
 
 # -- General configuration ---------------------------------------------------
